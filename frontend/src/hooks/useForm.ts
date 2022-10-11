ca8c64bdeec9134d0f5a8d9fcd3a3a743f3e5f41
deok2kim <dukgi77@naver.com>
2022-10-11 23:05:52 +0900

    refactor: Form hook 만들기

---
+++ frontend/src/hooks/useForm.ts
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+
+interface Validation {
+  required?: {
+    value: boolean;
+    message: string;
+  };
+  pattern?: {
+    value: string;
+    message: string;
+  };
+  custom?: {
+    isValid: (value: string) => boolean;
+    message: string;
+  };
+}
+type Validations<T extends {}> = Partial<Record<keyof T, Validation>>;
+type ErrorRecord<T> = Partial<Record<keyof T, string>>;
+
+export const useForm = <T extends Record<keyof T, any> = {}>(options?: {
+  initialValues?: Partial<T>;
+  validations?: Validations<T>;
+  onSubmit: () => void;
+}) => {
+  const [data, setData] = useState<T>((options?.initialValues || {}) as T);
+  const [errors, setErrors] = useState<ErrorRecord<T>>({});
+
+  const handleChange =
+    <S extends unknown>(key: keyof T, sanitizeFn?: (value: string) => S) =>
+    (e: React.ChangeEvent<HTMLInputElement & HTMLSelectElement>) => {
+      const value = sanitizeFn ? sanitizeFn(e.target.value) : e.target.value;
+      setData({
+        ...data,
+        [key]: value,
+      });
+    };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const validations = options?.validations;
+    if (validations) {
+      let valid = true;
+      const newErrors: ErrorRecord<T> = {};
+      for (const key in validations) {
+        const value = data[key];
+        const validation = validations[key];
+        if (validation?.required?.value && value) {
+          valid = false;
+          newErrors[key] = validation?.required?.message;
+        }
+
+        const pattern = validation?.pattern;
+        if (pattern?.value && !RegExp(pattern.value).test(value)) {
+          valid = false;
+          newErrors[key] = pattern.message;
+        }
+
+        const custom = validation?.custom;
+        if (custom?.isValid && !custom.isValid(value)) {
+          valid = false;
+          newErrors[key] = custom.message;
+        }
+      }
+
+      if (!valid) {
+        setErrors(newErrors);
+        return;
+      }
+    }
+
+    setErrors({});
+
+    if (options?.onSubmit) {
+      options.onSubmit();
+    }
+  };
+
+  return { data, errors, handleChange, handleSubmit };
+};
